227f4ef15f046c6b62f2f722f72654b754c575a3
Julian2224 <660.jr.22@gmail.com>
2025-05-22 19:08:47 -0300

    Trabajo Fizz Buzz finalizado, con el cambio de los numeros 3 a Fizz, 5 a Buzz y a 3 y 5 a FizzBuzz

--- Javascript_FizzBuzz/script.js
+++ Javascript_FizzBuzz/script.js
@@ -4,13 +4,25 @@ function fizzbuzz() {
     let resultado = document.getElementById("resultado");
 
     if (isNaN(numInput) || numInput < 1) {
-        resultado.innerHTML = "<li>Ingrese un número válido mayor que 0.</li>";
+        resultado.innerHTML = "<li>Ingrese un numero valido mayor que 0.</li>";
         return;
     }
     
     let numeros = "";
       for (let i = 1; i <= numInput; i++) {
-        numeros += i + " ";
+        if (i % 3 == 0 && i % 5 === 0){
+          numeros += "FizzBuzz ";
+        }
+        else if (i % 3 == 0){
+          numeros += "Fizz ";
+        }
+        else if (i % 5 === 0){
+          numeros += "Buzz "
+        }
+        else{
+          numeros += i + " ";
+        }
+        
       }
 
     resultado.textContent = numeros.trim();
